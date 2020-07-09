3a1b597f45d7e346be9a3026eb04412464d6508d
rahul <rahul2188@gmail.com>
2020-07-09 20:41:56 +0530

    Few changes

--- src/AssignmentFlipcart/FlipcartOrder.java
+++ src/AssignmentFlipcart/FlipcartOrder.java
@@ -57,10 +57,6 @@ public class FlipcartOrder {
 		Select maxValue=new Select(driver.findElement(By.xpath("//div[@class='_1YoBfV']/select")));
 		maxValue.selectByValue("60000");
 		
-		//WebElement element=
-				
-//		JavascriptExecutor js=(JavascriptExecutor)driver;
-//		js.executeScript("arguments[0].scrollIntoView()", driver.findElement(By.xpath("//div[text()='Resolution']")));
 		Thread.sleep(4000);
 		driver.findElement(By.xpath("//div[text()='Resolution']")).click();
 		driver.findElement(By.xpath("//div[contains(text(),'Ultra HD (4K)')]/preceding-sibling::div[@class='_1p7h2j']")).click();
@@ -88,28 +84,7 @@ public class FlipcartOrder {
 		File dest=new File("./screenshot/screenshot.png");
 		FileUtils.copyFile(src, dest);
 		
-		//selecting the lowest price
-//		List<WebElement> productsPrice=driver.findElements(By.xpath("//div[@class='_1vC4OE']"));
-//		List<WebElement> productNames=driver.findElements(By.xpath("//a[@class='_3YNWH1']"));
-//		String productName;
-//		String price;
-//		int intPrice;
-//		
-//		HashMap<String,Integer> hm=new HashMap<>();
-//		for(int i=1;i<productsPrice.size();i++) {
-//			productName=productNames.get(i).getText();
-//			price=productsPrice.get(i).getText().replaceAll("[^0-9]", "");
-//			intPrice=Integer.parseInt(price);
-//			hm.put(productName, intPrice);
-//			
-//		}
-//		
-//		Set<Entry<String,Integer>> set=hm.entrySet();
-//		
-//		for(Entry entry: set) {
-//			System.out.println(entry.getKey()+ "  "+entry.getValue());
-//			list.add((Integer) entry.getValue());
-//		}
+
 		List<WebElement> prices=driver.findElements(By.xpath("//div[@class='_37Mj_H']//div[@class='_1vC4OE']"));
 		ArrayList<Integer> list=new ArrayList<>();
 		for(int i=0;i<prices.size();i++) {
@@ -136,12 +111,19 @@ public class FlipcartOrder {
 		
 		Set<String> set=driver.getWindowHandles();
 		Iterator<String> id=set.iterator();
-		String parentWindow=id.next();
-		String childWindow=id.next();
-		driver.switchTo().window(childWindow);
-		System.out.println(driver.getTitle());
-		Thread.sleep(8000);
-		driver.findElement(By.xpath("//div[@class='_3aS5mM']//p[contains(text(),'Preorder')]")).click();
+		
+		for(String s:set) {
+			driver.switchTo().window(s);
+			if(driver.getTitle().contains("Flipkart Customer Care & Help Center")) {
+				driver.findElement(By.xpath("//div[@class='_3aS5mM']//p[contains(text(),'Preorder')]")).click();
+				System.out.println(driver.getTitle());
+				driver.findElement(By.xpath("//button[@class='_2AkmmA Gl6Vsq' and text()='Yes']")).click();
+				driver.findElement(By.xpath("//span[text()='Cart']")).click();
+				Thread.sleep(2000);
+				driver.findElement(By.xpath("//span[text()='Place Order']")).click();
+			}
+		}
+
 		
 		
 		
